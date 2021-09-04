a817f758a228677a65740ee295130a52becdacf5
zachary-newsom <48846154+zachary-newsom@users.noreply.github.com>
2021-09-03 22:08:16 -0700

    Done with s1c6

--- s1c6.cpp
+++ s1c6.cpp
@@ -34,6 +34,18 @@ struct byte_array
     }
 };
 
+struct byte_array_slice
+{
+    byte_t *bytes;
+    size_t length;
+
+    byte_array_slice(byte_array* other, size_t begin, size_t size)
+    {
+        length = size;
+        bytes = &other->bytes[begin];
+    }
+};
+
 const char *BASE_64_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 const char BASE_64_PADDING = '=';
 
@@ -42,10 +54,11 @@ int base64_char_to_hex(char ch)
     char *found = strchr(BASE_64_CHARACTERS, ch);
     if( found == NULL )
     {
-        printf("Padding!!!! %c\n", ch);
         return 0;
     }
-    return BASE_64_CHARACTERS - found+1; 
+    // printf("%02x ", found - BASE_64_CHARACTERS);
+
+    return found - BASE_64_CHARACTERS; 
 }
 
 // Base64 in string format
@@ -72,6 +85,13 @@ byte_array *base64_to_hex(byte_array *base64)
         hex->bytes[triplet_idx * 3 + 1] = triplet >> 8;
         hex->bytes[triplet_idx * 3 + 2] = triplet;
     }
+    
+    // remove off any padding
+    // for (int i = base64->length - 1; i >= 0; i--)
+    // {
+    //     if (base64->bytes[i] == '=')
+    //         hex->bytes--;
+    // }
 
     return hex;
 }
@@ -121,7 +141,7 @@ byte_array* byte_array_xor_char(byte_array *lhs, char rhs)
 byte_array* byte_array_xor(byte_array *lhs, byte_array *rhs)
 {
     size_t len = lhs->length;  
-    byte_array *arr = new byte_array(len );
+    byte_array *arr = new byte_array(len);
     int j = 0;
     for( int i = 0; i < len; i++ )
     {
@@ -131,6 +151,94 @@ byte_array* byte_array_xor(byte_array *lhs, byte_array *rhs)
 
     return arr;
 }
+
+
+size_t hamming_distance(byte_array *lhs, byte_array *rhs)
+{
+    size_t counter = 0;
+    size_t size = lhs->length > rhs->length ? lhs->length : rhs->length;
+    for ( size_t i = 0; i < size; i++ )
+    {
+        // result of xor is 1 if two bits are different
+        byte_t different = lhs->bytes[i] ^ rhs->bytes[i];
+
+        for ( size_t b = 0; b < 8; b++ )
+        {
+            if( (different >> b) & 0x1 != 0 )
+            {
+                counter++;
+            }
+        }
+    }
+
+    return counter;
+}
+
+size_t hamming_distance(byte_array_slice *lhs, byte_array_slice *rhs)
+{
+    size_t counter = 0;
+    size_t size = lhs->length > rhs->length ? lhs->length : rhs->length;
+    for ( size_t i = 0; i < size; i++ )
+    {
+        // result of xor is 1 if two bits are different
+        byte_t different = lhs->bytes[i] ^ rhs->bytes[i];
+
+        for ( size_t b = 0; b < 8; b++ )
+        {
+            if( (different >> b) & 0x1 != 0 )
+            {
+                counter++;
+            }
+        }
+    }
+
+    return counter;
+}
+
+
+
+// void single_char_xor(byte_array_slice& )
+// {
+//     int best_score = 0;
+//     byte_array *leading_arr;
+
+//     for ( int key = 0; key < 256; key++ )
+//     {
+//         // printf("%u - XOR'd with %c\n", key, key);
+
+//         byte_array *xored = byte_array_xor_char(hex_arr, (char)key);
+
+//         int score = 0;
+//         for( int i = 0; i < xored->length; i++ )
+//         {
+//             char checking = xored->bytes[i];
+//             if (checking == 'e' || checking == 'a' || checking == ' ') 
+//             {
+//                 score++;
+//             }
+//         }
+
+//         if (score > best_score)
+//         {
+//             leading_arr = xored;
+//             best_score = score;
+//         }
+//         else
+//         {
+//             delete xored;
+//         }
+
+//         // printf("%i - %s\n", key, xored[key]->bytes);
+//     }
+
+//     // printf("best guess - %s\n", xored[leading_arr]->bytes);
+    
+//     if (best_score > overall_best_score)
+//     {
+//         overall_best_score = best_score;
+//         overall_leading_arr = leading_arr;
+//     }
+// }
 
 int main(int argc, char* argv[])
 {
@@ -143,7 +251,7 @@ int main(int argc, char* argv[])
                     std::istreambuf_iterator<char>());
     base64.erase(std::remove(base64.begin(), base64.end(), '\n'), base64.end());
 
-    byte_array *base64_arr = str_to_byte_array(base64.c_str());
+    byte_array *base64_arr = str_to_byte_array(base64.c_str());    
 
     byte_array *hex = base64_to_hex(base64_arr);
     for (int i = 0; i < hex->length; i++ )
@@ -156,6 +264,88 @@ int main(int argc, char* argv[])
     //     printf("%02x", hex->bytes[i]);
     // }
     // printf("\n");
+
+    // key_size in bytes
+    double lowest = INFINITY;
+    size_t lowest_key_size = 0;
+    for( size_t key_size = 2; key_size <= 40; key_size++ )
+    {
+        size_t pair_count = hex->length / (key_size *2);
+
+        double sum = 0;
+        for (int i = 0; i < pair_count; i++)
+        {
+            byte_array_slice temp_a(hex, i*2*key_size, key_size);
+            
+            byte_array_slice temp_b(hex, i*2*key_size+key_size, key_size);
+            sum += double(hamming_distance(&temp_a, &temp_b)) / key_size;
+        }
+        auto result = sum / pair_count;
+
+        if (result < lowest)
+        {
+            lowest = result;
+            lowest_key_size = key_size;
+            printf("\nYo: %f %u", lowest, lowest_key_size);
+        }
+        else
+        {
+            printf("\n%u - %f", key_size, result);
+        }
+    }
+
+    size_t key_size = lowest_key_size;
+
+    size_t key_count = hex->length / (key_size * 2);
+
+    byte_array *key_arr = new byte_array(key_size);
+    for (size_t byte_pos = 0; byte_pos < key_size; byte_pos++)
+    {
+        size_t best_score = 0;
+        char leading_key;
+        for ( int ch_key = 0; ch_key < 256; ch_key++ )
+        {
+
+            byte_array* xored_arr = new byte_array(key_count);
+            for (size_t i = 0; i < key_count; i++)
+            {
+                auto temp = hex->bytes[i * key_size + byte_pos];
+
+                xored_arr->bytes[i] = temp ^ ch_key;
+            }
+
+            int score = 0;
+            for( int i = 0; i < xored_arr->length; i++ )
+            {
+                char checking = xored_arr->bytes[i];
+                //  We need a better heuristic
+                if (checking == 'e' || checking == 'E' || checking == 'a' || checking == 'A' || checking == 'O' || checking == 'o' || checking == 'i' || checking == 'I' || checking == ' ') 
+                {
+                    score++;
+                }
+            }
+
+            if (score > best_score)
+            {
+                leading_key = ch_key;
+                best_score = score;
+            }
+            else
+            {
+                delete xored_arr;
+            }
+        }
+
+        key_arr->bytes[byte_pos] = leading_key;
+    }
+
+    byte_array *stuff = byte_array_xor(hex, key_arr);
+
+    printf("\n");
+    for (int i = 0; i < key_arr->length; i++)
+    {
+        printf("%c", key_arr->bytes[i]);
+    }
 
     delete base64_arr;
     delete hex;
